caaab878a1e32b666bbb27df6676d1f0ee9cb8aa
rohit.jh <rohit.jh@media.net>
2019-06-17 23:35:09 +0530

    Changed exception

--- src/main/java/patterns/creational/factory/ComputerFactory.java
+++ src/main/java/patterns/creational/factory/ComputerFactory.java
@@ -1,11 +1,13 @@
 package main.java.patterns.creational.factory;
 
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
 /**
  * Created by rohit.jh on 12/06/19
  */
 
 public class ComputerFactory {
-    public Computer getComputer(String type) {
+    public Computer getComputer(String type) throws InvalidArgumentException {
         switch (type) {
             case "LAPTOP":
                 return new Laptop();
@@ -14,7 +16,7 @@ public class ComputerFactory {
             case "MICRO":
                 return new MicroComputer();
             default:
-                throw new ClassCastException();
+                throw new InvalidArgumentException(new String[]{});
         }
     }
 }
--- src/main/java/patterns/creational/factory/Main.java
+++ src/main/java/patterns/creational/factory/Main.java
@@ -1,11 +1,13 @@
 package main.java.patterns.creational.factory;
 
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
 /**
  * Created by rohit.jh on 12/06/19
  */
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidArgumentException {
         ComputerFactory computerFactory = new ComputerFactory();
 
         computerFactory.getComputer("LAPTOP").process();
